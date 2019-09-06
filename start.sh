10a5f03105d4296e7dadf062a26470a8fd9aa67f
myyong <myong@turing.ac.uk>
2019-09-06 13:16:39 +0100

    Add Python service and Datastore

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd wrattler/client
+export WRATTLER_LOCAL_STORAGE=True
+cd wrattler/server/data-store
+python app.py &
+cd ../python
+python app.py &
+cd ../../client
 http-server public -c-1 --cors=http://localhost:8889 &
+cd ../../
 jupyter lab --ip 0.0.0.0
